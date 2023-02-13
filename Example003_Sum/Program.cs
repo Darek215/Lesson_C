d9d6b0e70d860db2a9e919dbe143e15f69b2fd4c
Darek <Darek@mail.com>
2023-02-13 13:57:33 +0300

    Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int num1 = 4;
-int num2 = 6;
+﻿int num1 = new Random().Next(1, 10); // 1 2 3 4... 9
+Console.WriteLine(num1);
+int num2 = new Random().Next(1, 10);
+Console.WriteLine(num2);
 Console.WriteLine(num1 + num2);
